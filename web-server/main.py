c28f6e266c941ec9aa08962615aad813f788bd13
EstebanMa12 <desteban.maya@udea.edu.co>
2023-07-14 10:08:31 -0500

    A web server was built with the FastAPI library and maintained with Uvicorn// Requirements file update.

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,19 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app= FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3,4]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
+    return """ 
+        <h1>HOLA MUNDO DESDE UN SERVIDOR WEB UTILIZANDO PYTHON</h1>
+        <p>Este es un parrafo</p> """
+        
 
 def run():
     store.get_categories()
